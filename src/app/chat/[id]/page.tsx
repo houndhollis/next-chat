5e588bd05c2316f4aec3ad5171845988d5065e64
houndhollis <houndhollis9797@gmail.com>
2023-09-07 22:00:22 +0900

    feat: UserListItem.jsx 추가

--- src/app/chat/[id]/page.tsx
+++ src/app/chat/[id]/page.tsx
@@ -1,9 +1,26 @@
+'use client';
+
+import SideBar from '@/components/SideBar'
+import { useParams } from 'next/navigation'
 import React from 'react'
 
-const page = () => {
+const ChatPage = () => {
+
+  const { id } = useParams();
+
   return (
-    <div>page</div>
+    <main className="grid w-full grid-cols-8">
+      <div className="col-span-2">
+        <SideBar selectedChatId={id}/>
+      </div>
+      <div className="col-span-6 flex justify-center h-screen">
+        <div className='flex flex-col items-center justify-center space-y-4'>
+          {/* <IoChatbubblesOutline className='w-24 h-24 text-gray-300'/> */}
+          {/* <p className='text-2xl text-gray-300'>대화를 시작합니다</p> */}
+        </div>
+      </div>
+    </main>
   )
 }
 
-export default page
+export default ChatPage
--- src/components/UserListItem.tsx
+++ src/components/UserListItem.tsx
@@ -1,9 +1,40 @@
 'use client';
+import { IChat } from '@/types';
+import { User } from 'firebase/auth';
+import Image from 'next/image';
 import React from 'react'
 
-const UserListItem = () => {
+interface UserListItemProps {
+  sender: User
+  receiver: User,
+  chats: IChat[];
+  selectedChatId: string
+}
+
+const UserListItem = ({
+  sender,
+  receiver,
+  chats,
+  selectedChatId
+}: UserListItemProps) => {
   return (
-    <div>UserListItem</div>
+    <div className='w-full p-4'>
+      <div className='w-5/6 mx-auto px-4 flex flex-row items-center py-2 cursor-pointer'>
+        <div>
+          <Image
+            src={receiver?.photoURL}
+            width={40}
+            height={40}
+            alt='프로필'
+          />
+        </div>
+        <div className='ml-4'>
+          <p>
+            {receiver.displayName}
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
 
